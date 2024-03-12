feef28c8da8f4e4cff066b2086967407f8d2d788
HelenVr04 <helenvegaresendiz34@gmail.com>
2024-03-12 14:52:13 -0600

    contador-3

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -15,8 +15,8 @@ const Counter = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div>
-        <strong><h2 className="text-center">CONTADOR</h2></strong>
-        <p className="text-center">Número: - {count} -</p>
+        <strong><h2 className="text-center text-pink-500">CONTADOR</h2></strong>
+        <p className="text-center text-pink-500">Número: - {count} -</p>
         <button onClick={increment} className="bg-pink-500 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Incrementar</button>
         <button onClick={reset} className="bg-pink-500 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2">Reiniciar</button>
       </div>
